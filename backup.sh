a5907e531166fb91bcac0b003d4b652635cf8a06
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2015-08-13 11:07:53 +0200

    Add doc on how to restore

--- backup.sh
+++ backup.sh
@@ -3,3 +3,6 @@
 curl https://www.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
 # DTSTAMP will be changed every time. Drop it.
 grep -v '^DTSTAMP:' temp-sanrio_events_public.ics > sanrio_events_public.ics
+
+# When you want to restore the calendar, you must serve the ics file with a server which outputs utf-8 content-type header.
+# The easist way to achieve it is to use <https://rawgit.com/ohtake/backup-calendars/gh-pages/sanrio_events_public.ics> instead of <https://raw.githubusercontent.com/ohtake/backup-calendars/gh-pages/sanrio_events_public.ics> nor <http://ohtake.github.io/backup-calendars/sanrio_events_public.ics>.
