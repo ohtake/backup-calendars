cdf4bc5e06bb822884af37a13bd766e3f3cff16c
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2015-08-13 10:56:44 +0200

    Drop DTSTAMP because it changes every time

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl https://www.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o sanrio_events_public.ics
-
+curl https://www.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
+# DTSTAMP will be changed every time. Drop it.
+grep -v '^DTSTAMP:' temp-sanrio_events_public.ics > sanrio_events_public.ics
