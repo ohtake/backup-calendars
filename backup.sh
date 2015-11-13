68ac1f5ec43cebed52025b3ebf2eb906bc409bc8
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2015-11-14 01:23:52 +0900

    Host name has been changed

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://www.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
+curl https://calendar.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
 # DTSTAMP will be changed every time. Drop it.
 grep -v '^DTSTAMP:' temp-sanrio_events_public.ics > sanrio_events_public.ics
 
