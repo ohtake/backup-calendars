4435584e198bff22da408dfb9a614c9d32567824
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2019-05-23 08:59:02 +0900

    Use LF
    ICS files by Google have CRLF.
    Some versions of grep may output return codes as they are.

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 curl https://calendar.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
 # DTSTAMP will be changed every time. Drop it.
 grep -v '^DTSTAMP:' temp-sanrio_events_public.ics > sanrio_events_public.ics
+dos2unix sanrio_events_public.ics
 
 # When you want to restore the calendar, you must serve the ics file with a server which outputs utf-8 content-type header.
 # The easist way to achieve it is to use <https://rawgit.com/ohtake/backup-calendars/gh-pages/sanrio_events_public.ics> instead of <https://raw.githubusercontent.com/ohtake/backup-calendars/gh-pages/sanrio_events_public.ics> nor <http://ohtake.github.io/backup-calendars/sanrio_events_public.ics>.
