a36e83dcb509b2a58b1cc947b4adfb82a1e2d9ca
OHTAKE Tomohiro <tomohiro@kuramae.ne.jp>
2019-11-25 11:36:28 +0900

    Use temp directory
    This change prevents an error for "git add *.ics".

--- backup.sh
+++ backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-curl https://calendar.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp-sanrio_events_public.ics
+mkdir -p temp
+
+curl https://calendar.google.com/calendar/ical/qsqrk2emvnnvu45debac9dugr8%40group.calendar.google.com/public/basic.ics -o temp/sanrio_events_public.ics
 # DTSTAMP will be changed every time. Drop it.
-grep -v '^DTSTAMP:' temp-sanrio_events_public.ics > sanrio_events_public.ics
+grep -v '^DTSTAMP:' temp/sanrio_events_public.ics > sanrio_events_public.ics
 dos2unix sanrio_events_public.ics
 
 # When you want to restore the calendar, you must serve the ics file with a server which outputs utf-8 content-type header.
